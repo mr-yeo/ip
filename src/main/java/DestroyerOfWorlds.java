f951fbe850a822fe32a207454d97052b701d09b8
yeo <194688526+mr-yeo@users.noreply.github.com>
2026-08-18 20:38:39 +0800

    Rename Duke.java to DestroyerOfWorlds.java

--- src/main/java/DestroyerOfWorlds.java
+++ src/main/java/DestroyerOfWorlds.java
@@ -1,10 +1,11 @@
-public class Duke {
+public class DestroyerOfWorlds {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "\nHello I'm DESTROYEROFWORLDS\nPrepare to meet you DOOM!";
         System.out.println(banner);
     }
 }
